99f6970c8d89d64373e2dbaa16f55729add7309f
nhnahid1122 <mdxx0356@stud.kea.dk>
2024-04-06 02:00:13 +0200

    initial commit with navbar functionalities

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import '../styles/navbar.css';
+
+export default function Navbar() {
+  return (
+    <div className="navbar">
+      {/* logo */}
+      <div className="logoDiv">
+        <img src="src\assets\logo.png" alt="" />
+        <div className="title">
+          <h3>CAR</h3>
+          <p>Rental</p>
+        </div>
+      </div>
+
+      {/* links */}
+      <div className="linkDiv">
+        <a href="/#">Home</a>
+        <a href="/#">About</a>
+        <a href="/#">Vehicle Models</a>
+        <a href="/#">Testimonials</a>
+        <a href="/#">Our Team</a>
+        <a href="/#">Contacts</a>
+      </div>
+
+      {/* Buttons */}
+      <div className="buttonDiv">
+        <button type="button" className="signInBtn">Sign In</button>
+        <button type="button">Register</button>
+      </div>
+
+    </div>
+  );
+}
