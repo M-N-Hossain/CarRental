de59d277786b8fdf68900e6b3150050eb5342ce0
nhnahid1122 <mdxx0356@stud.kea.dk>
2024-06-29 02:50:12 +0200

    Made the navbar responsive

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,8 +1,22 @@
-import React from 'react';
+/* eslint-disable jsx-a11y/no-static-element-interactions */
+/* eslint-disable jsx-a11y/click-events-have-key-events */
+import React, { useState } from 'react';
 import '../styles/navbar.css';
 import logo from '../assets/logo.png';
 
 export default function Navbar() {
+  const [isMenuItemShowing, setIsMenuItemShowing] = useState(false);
+
+  const burgerMenuOpen = () => {
+    setIsMenuItemShowing(true);
+  };
+
+  const burgerMenuClose = () => {
+    setIsMenuItemShowing(false);
+  };
+
+  console.log(isMenuItemShowing);
+
   return (
     <div className="navbar">
       {/* logo */}
@@ -15,19 +29,33 @@ export default function Navbar() {
       </div>
 
       {/* links */}
-      <div className="linkDiv">
+
+      <div className={isMenuItemShowing ? 'linkDiv active' : 'linkDiv'}>
         <a href="/#">Home</a>
         <a href="/#">About</a>
         <a href="/#">Vehicle Models</a>
         <a href="/#">Testimonials</a>
         <a href="/#">Our Team</a>
         <a href="/#">Contacts</a>
+        {isMenuItemShowing && (
+        <>
+          <button type="button" className="signInBtn">Sign In</button>
+          <button type="button">Register</button>
+        </>
+        )}
       </div>
 
       {/* Buttons */}
       <div className="buttonDiv">
         <button type="button" className="signInBtn">Sign In</button>
         <button type="button">Register</button>
+      </div>
+
+      <div
+        className="burgerMenuIcons"
+        onClick={isMenuItemShowing ? burgerMenuClose : burgerMenuOpen}
+      >
+        <i className={isMenuItemShowing ? 'fas fa-times' : 'fas fa-bars'} />
       </div>
 
     </div>
